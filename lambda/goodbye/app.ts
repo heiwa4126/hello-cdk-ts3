c974924d600c23ee09262b5348e37e2585867e30
heiwa4126 <heiwa4126@gmail.com>
2024-11-22 14:50:29 +0900

    update

--- lambda/goodbye/app.ts
+++ lambda/goodbye/app.ts
@@ -1,6 +1,6 @@
 export const lambdaHandler = async (event, context) => {
 	return {
 		statusCode: 200,
-		body: "Hello, World!",
+		body: "Goodbye cruel World!",
 	};
 };
--- lib/hello-cdk-ts3-stack.ts
+++ lib/hello-cdk-ts3-stack.ts
@@ -1,4 +1,5 @@
 import cdk from "aws-cdk-lib";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 import iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction, OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs";
@@ -9,81 +10,48 @@ export class HelloCdkTs3Stack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		// Define a s3 bucket
-		const myBucket = new cdk.aws_s3.Bucket(this, "MyBucket", {
-			blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
-			versioned: false, // テストなのでバージョニング無効
-			// ↓テストなので削除しやすくする
-			removalPolicy: cdk.RemovalPolicy.DESTROY,
-			autoDeleteObjects: true, // lambda(とrole)が1個増えるけど便利
+		//---- Define a lambda function
+		const helloLambda = this.makeNodeLambda("hello");
+		const goodbyeLambda = this.makeNodeLambda("goodbye");
+
+		//---- Define API gateway
+		const gw = new apigw.RestApi(this, "apigw", {
+			restApiName: "Hello Service",
+			description: "This service serves hello.",
 		});
 
-		const utilLayer = new lambda.LayerVersion(this, "UtilLayer", {
-			code: lambda.Code.fromAsset("./layers/utilLayer"),
-			compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
-			description: "A utility layer with date-fns",
-		});
+		gw.root.addResource("hello").addMethod("GET", new apigw.LambdaIntegration(helloLambda));
+		gw.root.addResource("goodbye").addMethod("GET", new apigw.LambdaIntegration(goodbyeLambda));
 
-		const HelloS3Function = "HelloS3Function";
-		const myFunction = new NodejsFunction(this, HelloS3Function, {
-			entry: "lambda/hello/app.ts",
+		new cdk.CfnOutput(this, "endpoint", {
+			value: gw.url,
+		});
+	}
+
+	private makeNodeLambda = (functionName: string): NodejsFunction => {
+		const fn = new NodejsFunction(this, `${functionName}Function`, {
+			entry: `lambda/${functionName}/app.ts`,
 			runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
 			handler: "lambdaHandler",
-			layers: [utilLayer],
 			bundling: {
 				minify: true, // minifyオプションを有効にする
-				format: OutputFormat.ESM, // ES Modulesを使用する。`[Warning at /LearnHono6AwslambdaStack] If you are relying on AWS SDK v2 to be present in the Lambda environment already, please explicitly configure a NodeJS runtime of Node 16 or lower.`とか言われる。
-				// externalModules: ["'@aws-sdk/*'"], // AWS SDKは外部モジュールとして扱う（NODEJS_18_X以降のデフォルト） https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs-readme.html#externals
-				externalModules: ["@aws-sdk/*", "date-fns"], // AWS SDKとdate-fnsは外部モジュールとして扱う
+				format: OutputFormat.ESM,
+				// externalModules: ["@aws-sdk/*", "date-fns"], // AWS SDKとdate-fnsは外部モジュールとして扱う
 			},
-			role: new iam.Role(this, `${HelloS3Function}Role`, {
+			role: new iam.Role(this, `${functionName}Role`, {
 				assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
 				managedPolicies: [
 					iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"),
 				],
-				inlinePolicies: {
-					MyBucketWriteAccess: new iam.PolicyDocument({
-						statements: [
-							new iam.PolicyStatement({
-								actions: ["s3:PutObject"],
-								resources: [`${myBucket.bucketArn}/*`],
-							}),
-						],
-					}),
-				},
 			}),
-			environment: {
-				MyBucketName: myBucket.bucketName,
-				MyBucketRegion: this.region,
-			},
 		});
-		// 依存を明示する練習。myFunctionがmyBucketに依存することを明示。ほとんどの場合は不要。
-		myFunction.node.addDependency(myBucket);
-
-		// Define the Lambda function URL resource
-		const myFunctionUrl = myFunction.addFunctionUrl({
-			authType: lambda.FunctionUrlAuthType.NONE,
-		});
-
 		// Create a CloudWatch Logs Log Group for myFunction
-		const myFunctionLog = new logs.LogGroup(this, `${HelloS3Function}LogGroup`, {
-			logGroupName: `/aws/lambda/${myFunction.functionName}`,
+		new logs.LogGroup(this, `${functionName}LogGroup`, {
+			logGroupName: `/aws/lambda/${fn.functionName}`,
 			retention: logs.RetentionDays.ONE_WEEK,
 			removalPolicy: cdk.RemovalPolicy.DESTROY,
 		});
 
-		//--------------------------------------------------------------------
-		// Define a CloudFormation output for your URL
-		new cdk.CfnOutput(this, "MyBucketName", {
-			value: myBucket.bucketName,
-		});
-		// Define a CloudFormation output for your URL
-		new cdk.CfnOutput(this, "HelloFunctionUrl", {
-			value: myFunctionUrl.url,
-		});
-		// Define a CloudFormation output for LogGroup
-		new cdk.CfnOutput(this, "HelloFunctionLogGroup", {
-			value: myFunctionLog.logGroupName,
-		});
-	}
+		return fn;
+	};
 }
